10816cb5741ddcb817b083acc38ce8d975a29e37
ChoiYeoJin <sonata7531@gmail.com>
2024-04-12 22:17:16 +0900

    기본 컴포넌트

--- app/yard/page.tsx
+++ app/yard/page.tsx
@@ -1,9 +1,11 @@
+import Grass from "../components/map/Grass";
 import GrassHole from "../components/map/GrassHole";
 
 export default function Yard() {
   return (
     <>
       <GrassHole />
+      <Grass />
     </>
   );
 }
